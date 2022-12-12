2a4a98941d192512e2c893f7a9cebae032733f92
ruchi06 <ruchi06@gmail.com>
2022-12-12 09:10:51 +0530

    Pincode six digit

---
+++ Regex Pincode/Regex Pincode/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace Regex_Pincode
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter pincode :");
+            long pin = Convert.ToInt64(Console.ReadLine());
+            RegexPattern.Pattern(pin);
+
+        }
+    }
+}
---
+++ Regex Pincode/Regex Pincode/RegexPattern.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Regex_Pincode
+{
+    internal class RegexPattern
+    {
+        public static void Pattern(long pattern)
+        {
+            Regex code = new Regex("^[1-9]{1}[0-9]{5}$");
+            bool matches = code.IsMatch(Convert.ToString(pattern));
+
+            if (matches == true)
+            {
+                Console.WriteLine("Pncode is valid");
+            }
+            else
+            {
+                Console.WriteLine("Please Enter valid Pincode");
+            }
+        }
+    }
+}
